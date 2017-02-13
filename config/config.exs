c607a55a23a20f55ca90758c1364573db4170ff2
Johan Mattisson <johan.mattisson@gmail.com>
2017-02-13 17:55:50 +0100

    Added fetching, parsing and sorting of issues

--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,10 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+config :logger, backends: [:console], level: :info
+
+config :issues, github_url: "https://api.github.com"
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
